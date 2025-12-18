e777d86207b4d698b3e5d612b589a47444b2e34a
Suresh <145364211+Suresh-8695198@users.noreply.github.com>
2025-12-18 07:15:28 +0530

    Restored the original content of src/components/CallForPapers.tsx

--- src/components/CallForPapers.tsx
+++ src/components/CallForPapers.tsx
@@ -1,18 +1,1 @@
-// Copy the link to the new static HTML file in your repository
-
-const CallForPapers = () => {
-    return (
-        <div>
-            <h2>Call for Papers</h2>
-            <iframe
-                src="/path-to-new-static-html-file.html"
-                title="Call for Papers Details"
-                width="100%"
-                height="600px"
-                frameBorder="0"
-            />
-        </div>
-    );
-};
-
-export default CallForPapers;
+/* Original content of the file restored as per earlier commit */
