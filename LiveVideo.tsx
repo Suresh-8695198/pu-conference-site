d7c1eaa72c2be29ebc6ec027a5795ef41fd02b48
Suresh <145364211+Suresh-8695198@users.noreply.github.com>
2026-02-27 13:41:27 +0530

    Fix YouTube autoplay by updating the iframe src

---
+++ LiveVideo.tsx
@@ -0,0 +1,1 @@
+<iframe width="560" height="315" src="https://www.youtube.com/embed/d5cDPzDQ7eI?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
